ebf809f3c6239c4226045b9bceadac3c675cd3e6
Marc Webdev <bastien.marc.dev@gmail.com>
2022-09-21 10:22:26 -0400

    Intial Theme Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/*.{liquid, json}",
+    "./layout/*.{liquid, json}",
+    "./sections/*.{liquid, json}",
+    "./snippets/*.{liquid, json}",
+    "./templates/*.{liquid, json}",
+    "./templates/customers/*.{liquid, json}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
